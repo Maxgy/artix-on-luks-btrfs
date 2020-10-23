9e3dbbfae0a7962f72d3e8669928a6753ccf0f2d
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2020-10-22 20:20:24 -0600

    TODO: test the commands put into python

---
+++ iamchroot.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+
+import sys
+
+from subprocess import run, check_output
+
+# Input vars
+print("Region", end=": ")
+region = input().strip()
+print("City", end=": ")
+city = input().strip()
+
+disk = ""
+while True:
+    run("fdisk -l", shell=True)
+    print("\nDisk to install to", end=": ")
+    disk = input().strip()
+    if len(disk) > 0:
+        break
+
+# Boring stuff you should probably do
+#run(f"ln -sf /usr/share/zoneinfo/{region}/{city} /etc/localtime", shell=True)
+#run("hwclock --systohc", shell=True)
+#run("hwclock --systohc", shell=True)
+
+#run("pacman -Syu neovim bat fd exa ripgrep neofetch", shell=True)
+
+# Configure pacman
+print("Configure pacman as needed.", end="")
+input()
+#run("nvim /etc/pacman.conf", shell=True)
+#run("printf '\nkeyserver hkp://keyserver.ubuntu.com\n' >> /etc/pacman.d/gnupg/gpg.conf", shell=True)
+#run("pacman-key --populate artix", shell=True)
+
+# Localization
+print("Uncomment any locales need.", end="")
+input()
+#run("nvim /etc/locale.gen", shell=True)
+#run("locale-gen", shell=True)
+print("LANG", end="=")
+lang = input().strip()
+if len(lang) < 1:
+    lang = "en_US.UTF-8"
+
+print("KEYMAP", end="=")
+keymap = input().strip()
+if len(keymap) < 1:
+    keymap = "us"
+
+#run(f"printf 'LANG={lang}\n' > /etc/locale.conf", shell=True)
+#run(f"printf 'KEYMAP={keymap}\n' > /etc/vconsole.conf", shell=True)
+
+# Host stuff
+hostname = ""
+while True:
+    print("Hostname", end=": ")
+    hostname = input().strip()
+    if len(hostname) > 1:
+        break
+#run(f"printf '{hostname}\n' > /etc/hostname", shell=True)
+#run(f"printf '\n127.0.0.1\tlocalhost\n::1\t\tlocalhost\n127.0.1.1\t{hostname}.localdomain\t{hostname}\n' >> /etc/hosts", shell=True)
+
+# Install boot loader
+#run("pacman -S efibootmgr refind amd-ucode intel-ucode", shell=True)
+
+disk3uuid = check_output("sudo blkid /dev/mapper/cryptroot -o value -s UUID", shell=True).strip()
+#run(f"printf '\"Boot with standard options\"  \"cryptdevice=UUID={disk3uuid}:cryptroot root=/dev/mapper/cryptroot resume=/dev/mapper/cryptswap rootflags=subvol=@ rw initrd=amd-ucode.img initrd=intel-ucode.img initrd=initramfs-linux.img\"\n' > /boot/refind_linux.conf", shell=True)
+
+#run("refind-install", shell=True)
+#run(f"refind-install --usedefault {disk}1", shell=True)
+
+# Local.start
+#run(f"printf 'rfkill unblock wifi\nneofetch >| /etc/issue\n' > /etc/local.d/local.start", shell=True)
+#run("chmod +x /etc/local.d/local.start", shell=True)
+
+# Add default user
+#run("pacman -S zsh openrc-zsh-completions zsh-autosuggestions zsh-completions zsh-syntax-highlighting", shell=True)
+#run("chsh -s /bin/zsh", shell=True)
+#run("passwd", shell=True)
+#run("rm /etc/skel/.bash*", shell=True)
+#run("useradd -D -s /bin/zsh", shell=True)
+username = ""
+while True:
+    print("Username", end=": ")
+    username = input().strip()
+    if len(username) > 1:
+        break
+#run(f"useradd -m {username}", shell=True)
+#run(f"passwd {username}", shell=True)
+#run(f"usermod -a -G wheel {username}", shell=True)
+#run(f"usermod -a -G video {username}", shell=True)
+#run(f"usermod -a -G games {username}", shell=True)
+print("Uncomment the line to allow users of group wheel to execute any command.")
+input()
+#run("EDITOR=nvim visudo", shell=True)
+
+# Other stuff you should do or you'll be sad
+#run("pacman -S dhcpcd wpa_supplicant connman-openrc", shell=True)
+#run("rc-update add connmand", shell=True)
+#run("nvim /etc", shell=True)
+print("MOTD", end=": ")
+motd = input().strip()
+#run(f"printf '\n{motd}\n' > /etc/motd", shell=True)
+
+print("Remove duplicate 'subvol's from fstab.")
+print("And use '/dev/mapper/cryptroot instead of UUID.'")
+input()
+#run("printf '/dev/mapper/cryptswap\tswap\tswap\tdefaults\t0 0' >> /etc/fstab", shell=True)
+#run("nvim /etc/fstab", shell=True)
+
+# Finally fix swap
+swapuuid = check_output("sudo blkid /dev/mapper/cryptswap -o value -s UUID", shell=True).strip()
+#run("printf 'run_hook() {\n\tcryptsetup open /dev/disk/by-uuid/{swapuuid} cryptswap\n}\n' > /etc/initcpio/hooks/openswap", shell=True)
+#run("printf 'build() {\n\tadd_runscript\n}\n' > /etc/initcpio/install/openswap", shell=True)
+print("Add '/usr/bin/btrfs' to BINARIES")
+print("You might want to write this down:")
+print("  HOOKS=(... autodetect keyboard keymap modconf block encrypt openswap resume filesystems ...)")
+input()
+#run("nvim /etc/mkinitcpio.conf", shell=True)
+#run("mkinitcpio -P", shell=True)
+
+# Unmount and complete
+#run("umount -R /mnt", shell=True)
+
+print("\nTasks completed. You should reboot.")
--- install.py
+++ install.py
@@ -40,8 +40,8 @@ umount -R /mnt
 mount -o compress=zstd,subvol=@ /dev/mapper/cryptroot /mnt
 mkdir /mnt/.snapshots
 mkdir /mnt/home
-mount -o compress=zstd,subvol=@ /dev/mapper/cryptroot /mnt
-mount -o compress=zstd,subvol=@ /dev/mapper/cryptroot /mnt
+mount -o compress=zstd,subvol=@snapshots /dev/mapper/cryptroot /mnt/.snapshots
+mount -o compress=zstd,subvol=@home /dev/mapper/cryptroot /mnt/home
 mkdir /mnt/boot
 mount /dev/sdX1 /mnt/boot
 ---
@@ -86,6 +86,7 @@ nvim /etc/local.d/local.start
     neofetch >| /etc/issue
 chmod +x /etc/local.d/local.start
 ---
+passwd
 pacman -S zsh openrc-zsh-completions zsh-autosuggestions zsh-completions zsh-syntax-highlighting
 rm /etc/skel/.bash*
 useradd -D -s /bin/zsh
@@ -123,6 +124,112 @@ starship
 spacevim
 '''
 
-from subprocess import run
+import sys
 
-print("Installing Artix Linux with LUKS and Btrfs...")
+from subprocess import run, check_output
+
+print("Installing Artix Linux with LUKS and Btrfs...\n")
+
+# Check boot mode
+if len(check_output("ls /sys/firmware/efi/efivars", shell=True)) < 8:
+    print("\nNot booted in UEFI mode, aborting...")
+    sys.exit()
+
+# Load keymap
+print("Keymap", end=" (us): ")
+keymap = input().strip()
+if len(keymap) < 2:
+    keymap = "us"
+run(f"loadkeys {keymap}", shell=True)
+
+# Enable wireless
+run("rfkill unblock wifi", shell=True)
+run("ip link", shell=True)
+while True:
+    print("\nWireless interface", end=": ")
+    wlan = input().strip()
+    try:
+        check_output(f"ip link set {wlan} up", shell=True)
+        break
+    except:
+        pass
+
+# Connect to the internet
+while True:
+    print('''\nconnmanctl commands:
+        technologies
+        scan wifi
+        services
+        agent on
+        connect wifi_NAME
+        quit''')
+    run("connmanctl")
+
+    if "online" in str(check_output("connmanctl state", shell=True)):
+        break
+
+# Partition disk
+disk = ""
+while True:
+    run("fdisk -l", shell=True)
+    print("\nDisk to install to", end=": ")
+    disk = input().strip()
+    if len(disk) > 0:
+        break
+#run(f"cfdisk {disk}", shell=True)
+'''
+    gpt
+    1 New 1G 'EFI System'
+    2 New 16 GB 'Linux swap'
+    3 New *FREE 'Linux filesystem'
+    Write yes Quit
+'''
+
+# Setup encrypted partitions
+#run(f"cryptsetup -y -v luksFormat {disk}3", shell=True)
+#run(f"cryptsetup open {disk}3 cryptroot", shell=True)
+#run(f"cryptsetup -y -v luksFormat {disk}2", shell=True)
+#run(f"cryptsetup open {disk}2 cryptswap", shell=True)
+
+# Format partitions
+print("Label for swap", end=": ")
+swaplabel = input().strip()
+if len(swaplabel) < 1:
+    swaplabel = "myswap"
+#run(f"mkswap -L {swaplabel} /dev/mapper/cryptswap", shell=True)
+
+#run(f"mkfs.fat -F 32 {device}1", shell=True)
+
+print("Label for btrfs", end=": ")
+btrfslabel = input().strip()
+if len(btrfslabel) < 1:
+    btrfslabel = "btrfsroot"
+#run(f"mkfs.btrfs -L {btrfslabel} /dev/mapper/cryptroot", shell=True)
+
+# Create subvolumes
+#run("mount /dev/mapper/cryptroot /mnt", shell=True)
+#run("btrfs subvolume create /mnt/@", shell=True)
+#run("btrfs subvolume create /mnt/@snapshots", shell=True)
+#run("btrfs subvolume create /mnt/@home", shell=True)
+#run("umount -R /mnt", shell=True)
+
+# Mount subvolumes and boot
+#run("mount -o compress=zstd,subvol=@ /dev/mapper/cryptroot /mnt", shell=True)
+#run("mkdir /mnt/.snapshots", shell=True)
+#run("mkdir /mnt/home", shell=True)
+#run("mount -o compress=zstd,subvol=@snapshots /dev/mapper/cryptroot /mnt/.snapshots", shell=True)
+#run("mount -o compress=zstd,subvol=@home /dev/mapper/cryptroot /mnt/home", shell=True)
+#run("mkdir /mnt/boot", shell=True)
+#run(f"mount {disk}1 /mnt/boot", shell=True)
+
+# Install base system and kernel
+#run("basestrap /mnt base base-devel openrc cryptsetup btrfs-progs", shell=True)
+#run("basestrap /mnt linux linux-firmware linux-headers", shell=True)
+#run("fstabgen -U /mnt >> /mnt/etc/fstab", shell=True)
+
+# Chroot into new system
+#run("cp install.py /mnt/root/", shell=True)
+#run("cp iamchroot.py /mnt/root/", shell=True)
+print("\nRun iamchroot.py once you are in the new system.")
+input()
+#run("artools-chroot /mnt /bin/bash", shell=True)
